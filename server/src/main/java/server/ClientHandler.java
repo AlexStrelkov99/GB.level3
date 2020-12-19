f2d017b0ed602f7389884c30b7b7e2f4ad63dd47
AlexStrelkov99 <58663166+AlexStrelkov99@users.noreply.github.com>
2020-12-19 09:52:01 +0300

    Выполненная четвертая домашка третий уровень

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -22,103 +22,104 @@ public class ClientHandler {
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(() -> {
-                try {
-                    socket.setSoTimeout(120000);
-                    //цикл аутентификации
-                    while (true) {
-                        String str = in.readUTF();
-
-                        if (str.startsWith("/")) {
-                            if (str.startsWith("/reg ")) {
-                                String[] token = str.split("\\s", 4);
-                                boolean b = server.getAuthService()
-                                        .registration(token[1], token[2], token[3]);
-                                if (b) {
-                                    sendMsg("/regok");
-                                } else {
-                                    sendMsg("/regno");
-                                }
-                            }
-
-                            if (str.startsWith("/auth ")) {
-                                String[] token = str.split("\\s", 3);
-                                String newNick = server.getAuthService()
-                                        .getNicknameByLoginAndPassword(token[1], token[2]);
-                                if (newNick != null) {
-                                    login = token[1];
-                                    if (!server.isloginAuthenticated(login)) {
-                                        nickname = newNick;
-                                        out.writeUTF("/authok " + nickname);
-                                        server.subscribe(this);
-                                        socket.setSoTimeout(0);
-                                        break;
-                                    } else {
-                                        out.writeUTF("Учетная запись уже используется");
-                                    }
-                                } else {
-                                    out.writeUTF("Неверный логин / пароль");
-                                }
-                            }
-                        }
-                    }
-
-                    //Цикл работы
-                    while (true) {
-                        String str = in.readUTF();
-
-                        if (str.startsWith("/")) {
-                            if (str.startsWith("/w")) {
-                                String[] token = str.split("\\s+", 3);
-                                if (token.length < 3) {
-                                    continue;
-                                }
-                                server.privateMsg(this, token[1], token[2]);
-                            }
-
-                            if (str.equals("/end")) {
-                                out.writeUTF("/end");
-                                break;
-                            }
-                            if(str.startsWith("/chnick")){
-                                String[] token = str.split(" ", 2);
-                                if(token.length<2){
-                                    continue;
-                                }
-                                if (token[1].contains(" ")){
-                                    sendMsg("Не может содержать пробелов");
-                                    continue;
-                                }
-                                if(server.getAuthService().changeNick(this.nickname, token[1])){
-                                    sendMsg("/younickis " + token[1]);
-                                    sendMsg("Ваш ник изменен на " + token[1]);
-                                    this.nickname = token[1];
-                                    server.broadcastClientList();
-                                }
-                                else {
-                                    sendMsg("Не удалось изменить ник. Ник " + token[1] + " уже существует");
-                                }
-                            }
-
-
-                        } else {
-                            server.broadcastMsg(this, str);
-                        }
-                    }
-                }catch (SocketTimeoutException e){
-                    sendMsg("/end");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    System.out.println("Client disconnected!");
-                    server.unsubscribe(this);
+   //         new Thread(() -> {
+                server.getExecutorService().execute(() -> {
                     try {
-                        socket.close();
+                        socket.setSoTimeout(120000);
+                        //цикл аутентификации
+                        while (true) {
+                            String str = in.readUTF();
+
+                            if (str.startsWith("/")) {
+                                if (str.startsWith("/reg ")) {
+                                    String[] token = str.split("\\s", 4);
+                                    boolean b = server.getAuthService()
+                                            .registration(token[1], token[2], token[3]);
+                                    if (b) {
+                                        sendMsg("/regok");
+                                    } else {
+                                        sendMsg("/regno");
+                                    }
+                                }
+
+                                if (str.startsWith("/auth ")) {
+                                    String[] token = str.split("\\s", 3);
+                                    String newNick = server.getAuthService()
+                                            .getNicknameByLoginAndPassword(token[1], token[2]);
+                                    if (newNick != null) {
+                                        login = token[1];
+                                        if (!server.isloginAuthenticated(login)) {
+                                            nickname = newNick;
+                                            out.writeUTF("/authok " + nickname);
+                                            server.subscribe(this);
+                                            socket.setSoTimeout(0);
+                                            break;
+                                        } else {
+                                            out.writeUTF("Учетная запись уже используется");
+                                        }
+                                    } else {
+                                        out.writeUTF("Неверный логин / пароль");
+                                    }
+                                }
+                            }
+                        }
+
+                        //Цикл работы
+                        while (true) {
+                            String str = in.readUTF();
+
+                            if (str.startsWith("/")) {
+                                if (str.startsWith("/w")) {
+                                    String[] token = str.split("\\s+", 3);
+                                    if (token.length < 3) {
+                                        continue;
+                                    }
+                                    server.privateMsg(this, token[1], token[2]);
+                                }
+
+                                if (str.equals("/end")) {
+                                    out.writeUTF("/end");
+                                    break;
+                                }
+                                if (str.startsWith("/chnick")) {
+                                    String[] token = str.split(" ", 2);
+                                    if (token.length < 2) {
+                                        continue;
+                                    }
+                                    if (token[1].contains(" ")) {
+                                        sendMsg("Не может содержать пробелов");
+                                        continue;
+                                    }
+                                    if (server.getAuthService().changeNick(this.nickname, token[1])) {
+                                        sendMsg("/younickis " + token[1]);
+                                        sendMsg("Ваш ник изменен на " + token[1]);
+                                        this.nickname = token[1];
+                                        server.broadcastClientList();
+                                    } else {
+                                        sendMsg("Не удалось изменить ник. Ник " + token[1] + " уже существует");
+                                    }
+                                }
+
+
+                            } else {
+                                server.broadcastMsg(this, str);
+                            }
+                        }
+                    } catch (SocketTimeoutException e) {
+                        sendMsg("/end");
                     } catch (IOException e) {
                         e.printStackTrace();
+                    } finally {
+                        System.out.println("Client disconnected!");
+                        server.unsubscribe(this);
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
-            }).start();
+                    //          }).start();
+                });
         } catch (IOException e) {
             e.printStackTrace();
         }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,6 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     private ServerSocket server;
@@ -12,9 +14,15 @@ public class Server {
     private final int PORT = 8189;
     private List<ClientHandler> clients;
     private AuthService authService;
+    private ExecutorService executorService;
+
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
 
     public Server() {
         clients = new CopyOnWriteArrayList<>();
+        executorService = Executors.newCachedThreadPool();
      //   authService = new SimpleAuthService();
         if(!SQLHandler.connect()){
             throw new RuntimeException("Не удалось подключиться к БД");
